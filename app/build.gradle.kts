0423ab451ae906ce48595e26e01c0d144570c18e
pieczares <kupa.smierci123@gmail.com>
2026-04-28 17:49:30 +0200

    panel logowania

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
+    implementation("com.google.android.material:material:1.13.0")
 
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-compiler:2.51.1")
---
+++ app/src/main/java/com/dominox/clinicapp/ui/screens/login/LoginFragment.kt
@@ -0,0 +1,51 @@
+package com.dominox.clinicapp.ui.screens.login
+
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.dominox.clinicapp.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.buttonLogin).setOnClickListener {
+            var email = view.findViewById<TextInputEditText>(R.id.editEmail).text.toString().trim()
+            var password = view.findViewById<TextInputEditText>(R.id.editPassword).text.toString().trim()
+
+            var emailLayout = view.findViewById<TextInputLayout>(R.id.emailLayout)
+            var passwordLayout = view.findViewById<TextInputLayout>(R.id.passwordLayout)
+
+            emailLayout.error = null
+            passwordLayout.error = null
+
+            var isValid = true
+
+            if (email.isEmpty()){
+                emailLayout.error = "Podaj email"
+                isValid = false
+            }
+            else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                emailLayout.error = "Nieprawidłowy email"
+                isValid = false
+            }
+
+            if (password.isEmpty()){
+                passwordLayout.error = "Podaj hasło"
+                isValid = false
+            }
+
+            if (isValid){
+                Toast.makeText(requireContext(), "Logowanie OK", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.homeFragment)
+            }
+        }
+    }
+}
