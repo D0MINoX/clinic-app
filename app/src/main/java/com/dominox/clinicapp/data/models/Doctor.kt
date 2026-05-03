c99b67a3a975211bacc9c71ea2b1fcc8f1e72e04
Kominokis <pashaxd60@gmail.com>
2026-05-03 15:45:45 +0200

    Dodano lekarzy do spinnera oraz okno dialogowe wyboru daty i godziny

--- app/src/main/java/com/dominox/clinicapp/data/models/Doctor.kt
+++ app/src/main/java/com/dominox/clinicapp/data/models/Doctor.kt
@@ -2,8 +2,8 @@ package com.dominox.clinicapp.data.models
 
 data class Doctor(
     val id: Long,
-    val name: String,
+    val firstName: String,
+    val lastName: String,
     val specialization: String,
-    val phone: String,
-    val email: String
+    val isActive: Boolean
 )
