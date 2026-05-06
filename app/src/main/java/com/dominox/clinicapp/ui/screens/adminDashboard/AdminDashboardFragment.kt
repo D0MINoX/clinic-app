3d461bff7534e0bb24fcc6a7acaa7b762bbb4332
Kominokis <pashaxd60@gmail.com>
2026-05-06 20:14:10 +0200

    Dodano kafelek "Wizyty" przenoszący do ekranu wizyt lekarza

--- app/src/main/java/com/dominox/clinicapp/ui/screens/adminDashboard/AdminDashboardFragment.kt
+++ app/src/main/java/com/dominox/clinicapp/ui/screens/adminDashboard/AdminDashboardFragment.kt
@@ -6,8 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.dominox.clinicapp.R
 import com.dominox.clinicapp.api.TokenManager
+import com.dominox.clinicapp.ui.screens.home.HomeMenuAdapter
+import com.dominox.clinicapp.ui.screens.home.HomeMenuItem
 import com.google.android.material.card.MaterialCardView
 import dagger.hilt.android.AndroidEntryPoint
 import org.w3c.dom.Text
@@ -20,5 +25,22 @@ class AdminDashboardFragment : Fragment(R.layout.fragment_admin_dashboard) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<TextView>(R.id.adminWelcomeText).text = "Dzień dobry, ${tokenManager.getUserNameFromToken()}!"
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.dashboardRecyclerView)
+        val recyclerItems = listOf(
+            HomeMenuItem(
+                iconResId = android.R.drawable.ic_menu_agenda,
+                title = "Wizyty",
+                description = "Sprawdź zaplanowane wizyty",
+                destinationId = R.id.doctorAppointmentsFragment
+            )
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = HomeMenuAdapter(recyclerItems){ item ->
+            if(item.destinationId != null){
+                findNavController().navigate(item.destinationId)
+            }
+        }
     }
 }
