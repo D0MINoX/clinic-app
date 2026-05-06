b9096704b39e39ebf13f496b1c6796a4086b9a17
Kominokis <pashaxd60@gmail.com>
2026-05-06 20:16:15 +0200

    logika karty wizyt

---
+++ app/src/main/java/com/dominox/clinicapp/ui/screens/adminDashboard/appointments/DoctorAppointmentsFragment.kt
@@ -0,0 +1,12 @@
+package com.dominox.clinicapp.ui.screens.adminDashboard.appointments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dominox.clinicapp.R
+
+class DoctorAppointmentsFragment : Fragment(R.layout.fragment_doctor_appointments) {
+
+}
