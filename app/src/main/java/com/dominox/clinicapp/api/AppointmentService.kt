58bf88a8161f529203a97b0fbf21052c9ab3038b
Kominokis <pashaxd60@gmail.com>
2026-05-07 00:27:01 +0200

    optymalizacja importów

--- app/src/main/java/com/dominox/clinicapp/api/AppointmentService.kt
+++ app/src/main/java/com/dominox/clinicapp/api/AppointmentService.kt
@@ -4,9 +4,14 @@ import com.dominox.clinicapp.data.models.Appointment
 import com.dominox.clinicapp.data.models.AppointmentRequest
 import com.dominox.clinicapp.data.models.AppointmentResponse
 import com.dominox.clinicapp.network.NetworkClient
-import io.ktor.client.call.*
-import io.ktor.client.request.*
-import io.ktor.http.*
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import javax.inject.Inject
 
 class AppointmentService @Inject constructor() {
