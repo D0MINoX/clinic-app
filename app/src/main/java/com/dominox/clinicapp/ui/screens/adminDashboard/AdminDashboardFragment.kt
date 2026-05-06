4011ffd4e211f8f8dd5740c579ade1928ee95048
Kominokis <pashaxd60@gmail.com>
2026-05-06 11:43:54 +0200

    Dodano tekst witający admina

--- app/src/main/java/com/dominox/clinicapp/ui/screens/adminDashboard/AdminDashboardFragment.kt
+++ app/src/main/java/com/dominox/clinicapp/ui/screens/adminDashboard/AdminDashboardFragment.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.dominox.clinicapp.R
+import com.dominox.clinicapp.api.TokenManager
+import com.google.android.material.card.MaterialCardView
+import dagger.hilt.android.AndroidEntryPoint
+import org.w3c.dom.Text
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AdminDashboardFragment : Fragment(R.layout.fragment_admin_dashboard) {
+    @Inject lateinit var tokenManager: TokenManager
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_dashboard, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.adminWelcomeText).text = "Dzień dobry ${tokenManager.getUserNameFromToken()}!"
     }
 }
