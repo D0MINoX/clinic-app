6a1dd6db4b5aea390bdeb178b21912f7947dce30
D0MINoX <136051846+D0MINoX@users.noreply.github.com>
2026-05-15 20:22:03 +0200

    Implement real-time slot updates and refactor booking logic in BookingViewModel

--- app/src/main/java/com/dominox/clinicapp/network/NetworkClient.kt
+++ app/src/main/java/com/dominox/clinicapp/network/NetworkClient.kt
@@ -6,24 +6,27 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-
+import io.ktor.client.engine.okhttp.OkHttp
 object NetworkClient {
-    val httpClient = HttpClient{
+    val httpClient = HttpClient(OkHttp){
         install(ContentNegotiation){
             json(Json { ignoreUnknownKeys=true
             prettyPrint=true})
 
         }
         install(Logging) {
-            // To sprawi, że logi trafią do Logcata
             logger = object : Logger {
                 override fun log(message: String) {
                     Log.d("HTTP_CLIENT", message)
                 }
             }
             level = LogLevel.BODY
+        }
+        install(WebSockets) {
+            pingInterval = 15_000
         }
     }
 }
