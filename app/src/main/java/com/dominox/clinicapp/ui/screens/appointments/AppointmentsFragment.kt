6c5686e5097d10ff711a3e456797acfc99a1133a
Kominokis <pashaxd60@gmail.com>
2026-05-17 16:13:27 +0200

    Dodano funkcję generującą i pobierającą raport

--- app/src/main/java/com/dominox/clinicapp/ui/screens/appointments/AppointmentsFragment.kt
+++ app/src/main/java/com/dominox/clinicapp/ui/screens/appointments/AppointmentsFragment.kt
@@ -15,7 +15,9 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import io.ktor.http.ContentDisposition.Companion.File
 import kotlinx.coroutines.launch
+import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.inject.Inject
@@ -27,6 +29,8 @@ class AppointmentsFragment : Fragment(R.layout.fragment_appointments) {
     @Inject lateinit var tokenManager: TokenManager
     private val appointmentsAdapter = AppointmentsAdapter(emptyList())
     private val completedAppointmentsAdapter = AppointmentsAdapter(emptyList())
+    private var currentAppointments : List<Appointment> = emptyList()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -65,12 +69,23 @@ class AppointmentsFragment : Fragment(R.layout.fragment_appointments) {
         view.findViewById<Button>(R.id.bookAppointmentButton).setOnClickListener {
             findNavController().navigate(R.id.doctorsListFragment)
         }
+
+        // pobieranie raportu
+        view.findViewById<Button>(R.id.downloadReportButton)?.setOnClickListener {
+            if (currentAppointments.isNotEmpty()){
+                generateReport(currentAppointments)
+            }else{
+                Snackbar.make(requireView(), "Brak wizyt do pobrania", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
     private fun loadAppointments(id: Int) {
         viewLifecycleOwner.lifecycleScope.launch {
             val result = appointmentService.getPatientAppointments(id)
 
             result.onSuccess { list ->
+                currentAppointments = list
+
                 val (upcomingAppointments, completedAppointments) = splitAppointmentsByDate(list)
 
                 appointmentsAdapter.updateData(upcomingAppointments)
@@ -83,6 +98,41 @@ class AppointmentsFragment : Fragment(R.layout.fragment_appointments) {
                 Snackbar.make(requireView(), "Nie udało się pobrać wizyt", Snackbar.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun generateReport(appointments: List<Appointment>) {
+        val sb = StringBuilder()
+        sb.append("\tRAPORT WIZYT PACJENTA:\n")
+        sb.append("\t\tWygenerowano: ${LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))}\n")
+        sb.append("------------------------------------------------------------------------\n\n")
+
+        appointments.forEach { appointment ->
+            sb.append("Data: ${appointment.appointmentDate}\n")
+            sb.append("Godzina: ${appointment.appointmentTime}\n")
+            sb.append("Lekarz: ${appointment.doctorId}\n")
+            sb.append("Powod: ${appointment.reason}\n")
+            sb.append("Status: ${appointment.status}\n\n")
+            sb.append("------------------------------------------------------------------------\n\n")
+        }
+
+        try{
+            val fileName = "raport_wzyt_${System.currentTimeMillis()}.txt"
+            val storage = android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DOCUMENTS)
+
+            val appFolder = File(storage, "KlinikaZdrowia")
+            if(!appFolder.exists()) appFolder.mkdirs()
+
+            val file = File(appFolder, fileName)
+            file.writeText(sb.toString())
+
+            Snackbar.make(requireView(), "Pobrano raport: $fileName", Snackbar.LENGTH_LONG)
+                .setAction("OK") {}
+                .show()
+        }catch (e: Exception){
+            Snackbar.make(requireView(), "Wystąpił błąd podczas generowania raportu", Snackbar.LENGTH_LONG)
+        }
+
+
     }
 
     private fun splitAppointmentsByDate(appointments: List<Appointment>): Pair<List<Appointment>, List<Appointment>> {
